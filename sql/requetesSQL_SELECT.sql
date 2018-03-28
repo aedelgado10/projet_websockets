4f7c6cb8681cba4a9e75efddb268730c85a7ba15
unknown <william_slb@hotmail.com>
2018-03-28 15:38:04 +0200

    ajout des SQL, insertion de DATA et quelques requetes SELECT

---
+++ sql/requetesSQL_SELECT.sql
@@ -0,0 +1,14 @@
+#----------Recherche d'Activité
+
+SELECT nomA FROM Activite GROUP BY nomA  #Domaine
+SELECT typeA FROM Activite GROUP BY typeA  #Compétence dans le domaine
+SELECT nomA,typeA FROM Activite GROUP BY nomA #Les Deux
+
+#ID et Nom des Personnes avec le même horaire de dispo
+-- À REVOIR
+SELECT idPersonne, nomP
+FROM personne
+WHERE idPersonne = (SELECT e1.idPersonne
+					FROM etre_disponible as e1, etre_disponible as e2
+					WHERE e1.idCalendrier = e2.idCalendrier and e1.idPersonne != e2.idPersonne
+                    GROUP BY e1.idPersonne);
