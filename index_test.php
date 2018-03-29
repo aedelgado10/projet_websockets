0a8bd77934a9992d8d13218aca673168aab3bc58
unknown <willeMz31>
2018-03-29 20:48:45 +0200

    index improve

--- index_test.php
+++ index_test.php
@@ -4,7 +4,21 @@
  ?>
 
  	<!-- DÉBUT DU CONTENU DE LA PAGE-->
-	
+ 	<?php  
+ 		isset($_COOKIE["session"]) ? $logged = true : $logged = false;
+
+ 		if($logged){
+ 			echo "<br>";
+ 			echo "<h4> Voici l'activité que vous souhaitez apprendre </h4>";
+ 			echo "<h4> Voici l'activité dont vous êtes disponible pour enseigner</h4><br><br>";
+ 		}
+ 		else{
+ 			echo"<br>";
+ 			echo "<h4> Envie de découvrir et partager? Vous avez des connaissances et vous souhaitez apprendre d'avantage? </h4>";
+ 			echo "<h5> INSCRIVEZ VOUS GRATUITEMENT </h5>";
+ 			echo "<i> Le partage c'est du savoir </i><br><br>";
+ 		}
+ 	?>
 
 <?php 
     base_end();
